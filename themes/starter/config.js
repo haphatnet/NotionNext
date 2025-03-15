2e90831bdfe55f8407826e1ac48f7d2e98d90fa6
BRØAD <16910869+haphatnet@users.noreply.github.com>
2025-03-15 23:44:43 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -230,11 +230,11 @@ const CONFIG = {
 
   // 博客文章区块
   STARTER_BLOG_ENABLE: false, // 首页博文区块开关
-  STARTER_BLOG_TITLE: '我们的博客',
+  STARTER_BLOG_TITLE: '动态',
   STARTER_BLOG_COUNT: 3, // 首页博文区块默认展示前3篇文章
   STARTER_BLOG_TEXT_1: '最近的新闻',
   STARTER_BLOG_TEXT_2:
-    '这里会发布一些关于NotionNext的最新动态，包括新的动向、新的未来计划，以及新功能的特性',
+    '我们通过技术为品牌和公司创造数字体验。',
 
   // 联系模块
   STARTER_CONTACT_ENABLE: true, // 联系模块开关
