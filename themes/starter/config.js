70932ca23aa2fef02e7779bcc5cf14a3a61ed1aa
BRØAD <16910869+haphatnet@users.noreply.github.com>
2025-03-15 19:19:16 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -13,12 +13,12 @@ const CONFIG = {
   STARTER_HERO_TITLE_1: '通过技术为品牌和公司创造数字体验', // 英雄区文字
   STARTER_HERO_TITLE_2: '数字解决方案服务商', // 英雄区文字
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
-  STARTER_HERO_BUTTON_1_TEXT: '开始体验', // 英雄区按钮
-  STARTER_HERO_BUTTON_1_URL:
+  // STARTER_HERO_BUTTON_1_TEXT: '开始体验', // 英雄区按钮
+  // STARTER_HERO_BUTTON_1_URL:
     '#', // 英雄区按钮
-  STARTER_HERO_BUTTON_2_TEXT: 'Github', // 英雄区按钮
-  STARTER_HERO_BUTTON_2_URL: 'https://github.com/', // 英雄区按钮
-  STARTER_HERO_BUTTON_2_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
+  // STARTER_HERO_BUTTON_2_TEXT: 'Github', // 英雄区按钮
+  // STARTER_HERO_BUTTON_2_URL: 'https://github.com/', // 英雄区按钮
+  // STARTER_HERO_BUTTON_2_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
 
   // 英雄区配图，如需隐藏，改为空值即可 ''
   STARTER_HERO_PREVIEW_IMAGE: 'https://cdn.prod.website-files.com/5bff8886c3964a992e90d465/5c00621b7aefa4f9ee0f4303_wide-shot.svg', // 产品预览图 ，默认读取public目录下图片
@@ -200,9 +200,9 @@ const CONFIG = {
   // 团队成员区块
   STARTER_TEAM_ENABLE: true, // 团队成员区块开关
   STARTER_TEAM_TITLE: '团队成员',
-  STARTER_TEAM_TEXT_1: '我们的开发者团队',
+  STARTER_TEAM_TEXT_1: '我们的支援团队',
   STARTER_TEAM_TEXT_2:
-    'NotionNext 由众多开源技术爱好者们共同合作完成，感谢每一位<a className="underline" href="https://github.com/tangly1024/NotionNext/graphs/contributors">贡献者</a>',
+    'tiao.ma',
 
   // 这里不支持CONFIG和环境变量，需要一一修改此处代码。
   STARTER_TEAM_ITEMS: [
@@ -242,9 +242,9 @@ const CONFIG = {
   STARTER_CONTACT_TITLE: '联系我们',
   STARTER_CONTACT_TEXT: '告诉我们您遇到的问题',
   STARTER_CONTACT_LOCATION_TITLE: '我们的位置',
-  STARTER_CONTACT_LOCATION_TEXT: '中国，福建',
+  STARTER_CONTACT_LOCATION_TEXT: '中国',
   STARTER_CONTACT_EMAIL_TITLE: '我们如何帮助您？',
-  STARTER_CONTACT_EMAIL_TEXT: 'mail@tangly1024.com',
+  STARTER_CONTACT_EMAIL_TEXT: 'hi#haphat.net',
 
   // 嵌入外部表单
   STARTER_CONTACT_MSG_EXTERNAL_URL: 'https://noteforms.com/forms/yfctc7', // 基于NoteForm创建，将留言数据存在Notion中
